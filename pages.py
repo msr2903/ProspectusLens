b143c1933880a54a9cc71e1975e54e0f40e08bdd
Michael S. R. <89525199+msr2903@users.noreply.github.com>
2024-11-26 20:41:21 +0900

    Update pages.py

--- pages.py
+++ pages.py
@@ -7,6 +7,7 @@ def home():
     st.write("Welcome to the Prospectus Lens! Upload the PDF of the prospectus below!")
     uploaded_file = st.file_uploader("Upload your Prospectus File", accept_multiple_files=False, type=["pdf"])
     st.session_state["uploaded_file"] = uploaded_file
+    st.caption("Made with ❤️ by @michael_sr24")
 
 def cover():
     find_cover(uploaded_file=st.session_state.get("uploaded_file"))
